6f7f7c036d8b25a482e404f0237b5b69a48048ad
Matthew Little <zone117x@gmail.com>
2019-10-10 11:04:45 +0900

    Fix typings for each module output type, use umd for main field, remove browser field

---
+++ dist-esm/index.d.ts
@@ -0,0 +1,18 @@
+export declare type ByteArray = number[] | Uint8Array;
+export default class RIPEMD160 {
+    private _block;
+    private _blockSize;
+    private _blockOffset;
+    private _length;
+    private _finalized;
+    private _a;
+    private _b;
+    private _c;
+    private _d;
+    private _e;
+    constructor();
+    update(data: ByteArray): this;
+    private _update;
+    digest(): ByteArray;
+}
+//# sourceMappingURL=index.d.ts.map
